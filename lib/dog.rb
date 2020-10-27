c3b1766e1a55504a098f3bbc3a092e17a11abdfd
wickedvic <viksipart@gmail.com>
2020-10-27 10:33:42 -0400

    passed all tests

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+
+def name 
+    @name
+end
+
+def name=(name)
+    @name = name 
+end
+
+def breed 
+    @breed
+end
+
+
+def breed=(breed)
+    @breed = breed
+end
+
+
+
+
+
+
+
+
+end #end of dog class
